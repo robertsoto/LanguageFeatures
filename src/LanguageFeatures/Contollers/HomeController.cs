4136b041fb0b1b34cf59773f3a88a4f54e222626
Rob Soto <rob.soto@live.com>
2017-04-01 17:34:01 -0400

    Page 72 Nullable

--- src/LanguageFeatures/Contollers/HomeController.cs
+++ src/LanguageFeatures/Contollers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,7 +13,19 @@ namespace LanguageFeatures.Contollers
     {
         public ViewResult Index()
         {
-            return View(new string[] { "C#", "Language", "Features" });
+
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                //results.Add(string.Format("Name: {0}, Price: {1}", name, price));
+                string relatedName = p?.Related?.Name;
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",name, price, relatedName));
+            }
+
+
+            return View(results);
         }
     }
 }
